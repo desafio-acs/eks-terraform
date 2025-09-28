1890265a6725178e82dfb69c38ec70837fef9ba3
Samuel do Amaral Rodrigues <50115926+samuelamaralls@users.noreply.github.com>
2025-09-28 20:12:33 -0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -23,6 +23,11 @@ terraform {
   }
 }
 
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks_cluster.cluster_name
+  depends_on = [ module.eks_cluster ]
+}
+
 provider "aws" {
   region = var.region
 }
@@ -33,22 +38,14 @@ provider "cloudflare" {
 
 provider "kubernetes" {
   host                   = module.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
-    command     = "aws"
-  }
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster-auth.token
 }
 
 provider "helm" {
   kubernetes = {
-    host                   = module.eks_cluster.endpoint
-    cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
-    exec = {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
-      command     = "aws"
-    }
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster-auth.token
   }
 }
